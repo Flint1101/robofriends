9d8013289006f6e2d7cc458bf67b048d3f5040cb
Flint1101 <maxim.dudai11@gmail.com>
2022-05-15 18:38:46 +0100

    some scrollbar changes

--- src/components/Scroll/Scroll.js
+++ src/components/Scroll/Scroll.js
@@ -1,10 +1,12 @@
 import React from "react";
+import './scroll.css';
 
 const Scroll = (props) => {
     return (
         <div style={{
             overflowY: 'scroll',
-            height: '500px'
+            height: '40em',
+            margin: '40px 0 0 0'
         }}>
             {props.children}
         </div>
